fc6d16f56d392ba58d7957a73c95ae6fd7a120d8
vitoriadullius <vitoria.dullius.lopes@gmail.com>
2023-05-22 14:18:33 -0500

    Added Odd & Even task

---
+++ src/CodingTasks/OddAndEven.java
@@ -0,0 +1,12 @@
+package CodingTasks;
+
+public class OddAndEven {
+    public static void main(String[] args) {
+        /*
+        Write a method which can identifies if given number is even or odd
+        EX:
+               identify(5) -> "odd"
+               identify(6) -> "Even"
+         */
+    }
+}
