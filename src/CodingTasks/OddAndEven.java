c557762e4a6679fb9fc29dc9c9a3c920c2ad2478
Milan <petrovicmilan82@gmail.com>
2023-05-24 13:05:27 -0500

    OddEven task is done

--- src/CodingTasks/OddAndEven.java
+++ src/CodingTasks/OddAndEven.java
@@ -2,11 +2,24 @@ package CodingTasks;
 
 public class OddAndEven {
     public static void main(String[] args) {
+
         /*
         Write a method which can identifies if given number is even or odd
         EX:
                identify(5) -> "odd"
                identify(6) -> "Even"
+
+
          */
+
+        int num = 15;
+
+
+        if (num % 2 == 0) {
+            System.out.println("Even number");
+        } else {
+            System.out.println("Odd number");
+        }
+
     }
 }
