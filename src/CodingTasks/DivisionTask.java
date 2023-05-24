f7ec242f255c3550f119e119afb0de7385a3d271
Milan <petrovicmilan82@gmail.com>
2023-05-24 13:04:35 -0500

    DivisionTask is done

--- src/CodingTasks/DivisionTask.java
+++ src/CodingTasks/DivisionTask.java
@@ -6,5 +6,24 @@ public class DivisionTask {
         Divide without this operator: /
         Write a method that can divide two numbers without using division operator
          */
+
+
+        int n1 = 13;
+        int n2 = 4;
+        int result= 0;
+
+        while ((n1 >= n2)){
+            n1 = n1 -n2;
+            result ++;
+
+        }
+
+        System.out.println(result);
+
+        System.out.println("Remainder is: " + n1);
+
+
+
+
     }
 }
