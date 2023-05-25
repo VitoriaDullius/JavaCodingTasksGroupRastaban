6be8307982d4326a6d4478c46bca3f12b286e33c
vitoriadullius <vitoria.dullius.lopes@gmail.com>
2023-05-25 12:30:10 -0500

    Finra class created

---
+++ src/CodingTasks/vitoria/week1_Tasks/Finra_Task.java
@@ -0,0 +1,12 @@
+package CodingTasks.vitoria.week1_Tasks;
+
+public class Finra_Task {
+    public static void main(String[] args) {
+        /*
+        Write a method which prints out the numbers from 1 to 30. But for numbers which
+        are a multiple of 3, print "FIN" instead of the number.
+        And for numbers which are a multiple of 5, print "RA" instead of the number.
+        For numbers which are a multiple of both 3 and 5, print "FINRA" instead of the number.
+         */
+    }
+}
