1503bca20471ac33577fbbf9a9abc22a62e17628
vitoriadullius <vitoria.dullius.lopes@gmail.com>
2023-05-25 13:03:15 -0500

    Updated FinraTask

--- src/CodingTasks/vitoria/week1_Tasks/Finra_Task.java
+++ src/CodingTasks/vitoria/week1_Tasks/Finra_Task.java
@@ -8,5 +8,17 @@ public class Finra_Task {
         And for numbers which are a multiple of 5, print "RA" instead of the number.
         For numbers which are a multiple of both 3 and 5, print "FINRA" instead of the number.
          */
+        String result = "";
+        for(int i=1; i <= 30; i++) {
+            if(i % 5==0 && i %3 ==0)
+                result += "FINRA ";
+            else if(i%5 == 0)
+                result += "RA ";
+            else if(i%3==0)
+                result+="FIN ";
+            else
+                result += i+" ";
+        }
+        System.out.println(result);
     }
 }
