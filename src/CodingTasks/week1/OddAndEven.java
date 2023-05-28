f5188055c51d5f4f8ca1f5dd8258775b1c393623
Ahiabwi <waddo2017@gmail.com>
2023-05-28 14:02:44 -0500

    added odd and even method

--- src/CodingTasks/week1/OddAndEven.java
+++ src/CodingTasks/week1/OddAndEven.java
@@ -1,12 +1,28 @@
-package CodingTasks;
+package CodingTasks.week1;
 
 public class OddAndEven {
     public static void main(String[] args) {
+
+        int x = 5;
+
+        // x = 6;
+
+        if (x % 2 == 1) {
+
+            System.out.println("Odd");
+
+        } else {
+
+            System.out.println("Even");
+        }
+    }
+}
+
+
+
         /*
         Write a method which can identifies if given number is even or odd
         EX:
                identify(5) -> "odd"
                identify(6) -> "Even"
          */
-    }
-}
