0877b79f4286dafccf95305d0134701d498a6df2
ibtissam <bekkaouiibtissam@gmail.com>
2023-05-29 19:08:07 -0400

    update divisibleBy  and divide without division operator

--- src/CodingTasks/ibtissam/DivWithoutDivOpe.java
+++ src/CodingTasks/ibtissam/DivWithoutDivOpe.java
@@ -4,7 +4,7 @@ public class DivWithoutDivOpe {
 
     public static void main(String[] args) {
 
-        int a = 50;
+      /*  int a = 50;
         int b = 9;
 
         int count = 0;
@@ -15,5 +15,25 @@ public class DivWithoutDivOpe {
         }
         System.out.println(count + " with remainder " + a);
 
+    */
+
+      divisibleWithoutDivOper(30,6);
+
+
+
     }
+
+    public static void divisibleWithoutDivOper(int a, int b){
+
+        int count = 0;
+
+        while (a >= b){
+            a -= b;
+            count++;
+        }
+        System.out.println(count + " with remainder " + a);
+
+    }
+
+
 }
--- src/CodingTasks/ibtissam/DivisibleBy3515.java
+++ src/CodingTasks/ibtissam/DivisibleBy3515.java
@@ -4,9 +4,9 @@ public class DivisibleBy3515 {
 
     public static void main(String[] args) {
 
-        int n = 100;
+        int n = 200;
 
-        String divisibleBy15 = "",
+      /*  String divisibleBy15 = "",
                 divisibleBy5 = "",
                 divisibleBy3 = "";
 
@@ -26,13 +26,37 @@ public class DivisibleBy3515 {
         System.out.println("DivisibleBy15: " + divisibleBy15);
         System.out.println("DivisibleBy5: " + divisibleBy5);
         System.out.println("DivisibleBy3: " + divisibleBy3);
+*/
 
-
-
-
-
+        divisibleBy(n);
 
     }
+
+    public static void divisibleBy(int n){
+
+        String divisibleBy15 = "", divisibleBy5 = "", divisibleBy3= "";
+
+        for (int i = 1; i <= n ; i++){
+            if (i % 15 == 0  &&  i % 3 == 0){
+                divisibleBy15 += i + " ";
+            }
+            if (i % 5 == 0  && i% 15 != 0){
+                divisibleBy5 += i + " ";
+            }
+            if (i% 3 == 0  && i% 15 != 0){
+               divisibleBy3 += i  + " ";
+            }
+        }
+        System.out.println("Divisble by 15: " + divisibleBy15);
+        System.out.println("Divisble by 15: " + divisibleBy5);
+        System.out.println("Divisble by 15: " + divisibleBy3);
+    }
+
+
+
+
+
+
 }
 /*
 Write a program that can print the numbers between 1~100 that can be divisible by 3, 5 and 15.
